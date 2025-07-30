a41783f1458f5a8cf0267f281a39f9e4ef146813
AnJia <anjia0532@gmail.com>
2025-07-30 15:22:39 +0800

    feat(com.alibaba.android.rimet):优化钉钉快捷打卡功能
    - 修改应用名称为"钉钉"- 添加新的匹配规则，提高打卡准确性
    - 调整规则顺序和参数，优化用户体验

--- src/apps/com.alibaba.android.rimet.ts
+++ src/apps/com.alibaba.android.rimet.ts
@@ -2,24 +2,39 @@ import { defineGkdApp } from '@gkd-kit/define';
 
 export default defineGkdApp({
   id: 'com.alibaba.android.rimet',
-  name: 'GKD',
+  name: '钉钉',
   groups: [
     {
       key: 1,
       name: '功能类 - 快捷打卡',
+      desc: '功能类 - 快捷打卡',
       matchRoot: true,
       actionMaximum: 1,
-      order: 1,
+      order: 10,
       matchTime: 10000,
       resetMatch: 'app',
       rules: [
         {
           key: 1,
           fastQuery: true,
-          name: '进入工作台',
           activityIds:
             'com.alibaba.lightapp.runtime.ariver.TheOneActivityMainTask',
           matches: '[vid="home_app_item" && clickable=true && desc="工作台"]',
+        },
+        {
+          key: 2,
+          preKeys: [1],
+          fastQuery: true,
+          matchTime: 10000,
+          actionDelay: 5000,
+          actionCd: 5000,
+          activityIds:
+            'com.alibaba.lightapp.runtime.ariver.TheOneActivityMainTask',
+          matches: '[vid="common_webview" && clickable=true]',
+          position: {
+            top: 'height * 0.52',
+            left: 'width * 0.126',
+          },
         },
       ],
     },
