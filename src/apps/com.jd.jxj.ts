dba8597216bc3c08134ad28c7744543ee9aca814
AnJia <anjia0532@gmail.com>
2025-08-29 11:44:11 +0800

    feat: 更新京粉规则
    - 新增京粉应用规则，增加全屏广告-弹框广告拦截
    - 更新 CHANGELOG.md 和 README.md，反映规则变更
    -将版本号从 v9 升级到 v10

---
+++ src/apps/com.jd.jxj.ts
@@ -0,0 +1,21 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.jd.jxj',
+  name: '京粉',
+  groups: [
+    {
+      key: 7,
+      name: '全屏广告-弹框广告',
+      fastQuery: true,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: [
+        {
+          fastQuery: true,
+          matches: '[vid="popup_close_imgid"]',
+        },
+      ],
+    },
+  ],
+});
